6470e69444a3c78c5c373864a00665cba835365f
Prince <princekizhthara@gmail.com>
2026-04-03 13:19:37 +0530

    feat: Add release signing configuration and update .gitignore for keystore files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,11 +20,17 @@ android {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("../../vaultcash-release.jks")
+            storePassword = "vaultcash2026"
+            keyAlias = "vaultcash"
+            keyPassword = "vaultcash2026"
+        }
+    }
+
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.studio63.vaultcash"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -33,9 +39,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
